81be2793d42fb472378c24cf9dcaf19158152c36
Shikazu777 <dhanrax@gmail.com>
2025-07-07 19:18:25 +0530

    code updated

--- javascript/js2.js
+++ javascript/js2.js
@@ -6,10 +6,25 @@ console.log(name,since)
 
 //es6
 //let,const
-
+let y;
 let emlpoyee='mohan'
 let age=20
 let city='chennai'
 // let name;
 name='parmod'
 console.log(emlpoyee,age,city)
+
+const x={
+    x:7
+};
+
+console.log(x)
+
+const cities=['chennai','trichy'];
+{
+    let city='chennai';
+    console.log(city)
+    let cities='trichy';
+    console.log(cities)
+}
+
