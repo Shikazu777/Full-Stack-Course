6b12a637c2d76ef51f40397ae8fcf009f92b96cf
Shikazu777 <dhanrax@gmail.com>
2025-07-07 18:58:05 +0530

    code updated

--- javascript/js1.js
+++ javascript/js1.js
@@ -2,6 +2,6 @@ console.log('js-1 in external file')
 console.error('no error')
 console.warn('warning')
 console.table({
-    "name":"guvi",
+    "name":"dhanraj",
     "since":2014
 })
---
+++ javascript/js2.js
@@ -0,0 +1,15 @@
+var name;
+name='dhanraj'
+name='nagarajan'
+var since=2005
+console.log(name,since)
+
+//es6
+//let,const
+
+let emlpoyee='mohan'
+let age=20
+let city='chennai'
+// let name;
+name='parmod'
+console.log(emlpoyee,age,city)
