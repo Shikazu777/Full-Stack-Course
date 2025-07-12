a35c6fd66685bb7be3ece4c68851c5a75407fe50
Shikazu777 <dhanrax@gmail.com>
2025-07-12 12:51:12 +0530

    code updated

---
+++ javascript/js9.js
@@ -0,0 +1,19 @@
+// for loop
+// The for loop is one of the most commonly used loops in JavaScript. It
+// consists of three main parts: initialization, condition, and increment/
+// decrement.
+
+// for (initialization; condition; increment/decrement) {
+// // code block to be executed
+// }
+
+// for(let i=0;i<5;i++){
+// // console.log(i)
+// }
+
+// while loop: The while loop executes as long as a specified condition is
+// true.
+
+// while (condition) {
+// // . . . . . // code block to be executed
+// }
