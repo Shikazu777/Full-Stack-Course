a103628bd566b9a0295ca36e0f6d410c2db22ba9
Shikazu777 <dhanrax@gmail.com>
2025-07-12 19:17:21 +0530

    code updated

---
+++ javascript/js10.js
@@ -0,0 +1,9 @@
+// The forEach method in JavaScript is used to execute a provided function once for each array
+// element. It is a higher-order function and part of the array prototype, allowing you to perform
+// actions directly on array elements without the need for explicit indexing.
+// 
+
+// array.forEach(function(currentValue, index, array) {
+// // code block to be executed
+// });
+
